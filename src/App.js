dc80a67e05587d1b2f975d4edb5cd39f66a6db47
raulnicolasagusto <raulnicolasagusto@gmail.com>
2024-12-14 20:09:56 -0300

    crudReactNode

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="datos">
+        <label>Nombre:<input type="text"/></label><br/>
+        <label>Edad:<input type="text"/></label><br/>
+        <label>Pais:<input type="text"/></label><br/>
+        <label>Cargo:<input type="text"/></label><br/>
+        <label>Años:<input type="text"/></label><br/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
