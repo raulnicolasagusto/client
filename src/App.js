d4bf534dac18355c99ac51555578905430b9cc1b
raulnicolasagusto <raulnicolasagusto@gmail.com>
2024-12-19 20:58:33 -0300

    conectado a la base de datos

--- src/App.js
+++ src/App.js
@@ -1,38 +1,71 @@
 import './App.css';
-import { useState} from 'react'
+import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
-
-  const [nombre, setNombre] = useState("");
+  const [nombre, setNombre] = useState('');
   const [edad, setEdad] = useState(0);
-  const [pais, setPais] = useState("");
-  const [cargo, setCargo] = useState("");
+  const [pais, setPais] = useState('');
+  const [cargo, setCargo] = useState('');
   const [anios, setAnios] = useState(0);
 
-  const mostrarDatos = ()=>{
-    alert(nombre);
-  }
+  const add = () => {
+    Axios.post('http://localhost:3001/create', {
+      nombre: nombre,
+      edad: parseInt(edad), // Convertir a número
+      pais: pais,
+      cargo: cargo,
+      anios: parseInt(anios), // Convertir a número
+    })
+      .then(() => {
+        console.log('Empleado registrado');
+      })
+      .catch((error) => {
+        console.error('Error al registrar empleado:', error);
+      });
+  };
 
   return (
-    
     <div className="App">
       <div className="datos">
-        <label>Nombre:<input onChange={(event)=>{
-          setNombre(event.target.value);
-        }} type="text"/></label>
-        <label>Edad:<input onChange={(event)=>{
-          setEdad(event.target.value);
-        }}  type="text"/></label>
-        <label>Pais:<input onChange={(event)=>{
-          setPais(event.target.value);
-        }}  type="text"/></label>
-        <label>Cargo:<input onChange={(event)=>{
-          setCargo(event.target.value);
-        }}  type="text"/></label>
-        <label>Años:<input onChange={(event)=>{
-          setAnios(event.target.value);
-        }}  type="text"/></label>
-        <button onClick={mostrarDatos} className="">Registrar</button>
+        <label>
+          Nombre:
+          <input
+            onChange={(event) => setNombre(event.target.value)}
+            type="text"
+          />
+        </label>
+        <label>
+          Edad:
+          <input
+            onChange={(event) => setEdad(event.target.value)}
+            type="number"
+          />
+        </label>
+        <label>
+          País:
+          <input
+            onChange={(event) => setPais(event.target.value)}
+            type="text"
+          />
+        </label>
+        <label>
+          Cargo:
+          <input
+            onChange={(event) => setCargo(event.target.value)}
+            type="text"
+          />
+        </label>
+        <label>
+          Años:
+          <input
+            onChange={(event) => setAnios(event.target.value)}
+            type="number"
+          />
+        </label>
+        <button onClick={add} className="">
+          Registrar
+        </button>
       </div>
     </div>
   );
